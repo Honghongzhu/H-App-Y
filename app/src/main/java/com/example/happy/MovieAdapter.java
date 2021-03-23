ac06f45a95bdd87c8a96c80b22e205b453af101d
Honghongzhu <honghong033@hotmail.com>
2021-03-23 16:49:24 +0100

    Top10 screen (RecyclerView)

---
+++ app/src/main/java/com/example/happy/MovieAdapter.java
@@ -0,0 +1,53 @@
+package com.example.happy;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
+    private final LinkedList<String> moviesToAdapt;
+    private final LayoutInflater inflater;
+
+    public class MovieViewHolder extends RecyclerView.ViewHolder{
+        //TODO: add the corresponding movie images from database, now it's a default
+        public final TextView movieText;
+        final MovieAdapter adapter;
+
+        public MovieViewHolder(View view, MovieAdapter adapter){
+            super(view);
+            movieText = view.findViewById(R.id.movieName);
+            this.adapter = adapter;
+        }
+    }
+
+    public MovieAdapter(Context context, LinkedList<String> moviesToAdapt){
+        inflater = LayoutInflater.from(context);
+        this.moviesToAdapt = moviesToAdapt;
+    }
+
+    @NonNull
+    @Override
+    public MovieAdapter.MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.movie_item, parent, false);
+        return new MovieViewHolder(view, this);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieAdapter.MovieViewHolder holder, int position) {
+        String movieAtPosition = moviesToAdapt.get(position);
+        holder.movieText.setText(movieAtPosition); //TODO: get from database
+    }
+
+    @Override
+    public int getItemCount() {
+        return moviesToAdapt.size();
+    }
+}
--- app/src/main/java/com/example/happy/Top10Activity.java
+++ app/src/main/java/com/example/happy/Top10Activity.java
@@ -2,12 +2,34 @@ package com.example.happy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class Top10Activity extends AppCompatActivity {
+    private LinkedList<String> moviesToAdapt = new LinkedList<>();
+    private RecyclerView recyclerView;
+    private MovieAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top10);
+
+        // This is dummy code to display movie names
+        for (int i = 0; i < 10; i++) {
+            moviesToAdapt.addLast("Movie " + (i+1));
+        }
+
+        // Get a handle to the RecyclerView
+        recyclerView = findViewById(R.id.rv_top10);
+        // Create an adapter and supply the data to be displayed
+        adapter = new MovieAdapter(this, moviesToAdapt);
+        // Connect the adapter with the RecyclerView
+        recyclerView.setAdapter(adapter);
+        // Give the RecyclerView a default layout manager
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
