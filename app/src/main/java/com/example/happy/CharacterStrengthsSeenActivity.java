22554be42504b25a89a1d18e4c2361b035422acc
Honghongzhu <honghong033@hotmail.com>
2021-03-23 14:50:05 +0100

    add backarrows to mainscreen

--- app/src/main/java/com/example/happy/CharacterStrengthsSeenActivity.java
+++ app/src/main/java/com/example/happy/CharacterStrengthsSeenActivity.java
@@ -3,11 +3,11 @@ package com.example.happy;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CharacterStrenghtsSeenActivity extends AppCompatActivity {
+public class CharacterStrengthsSeenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_character_strenghts_seen);
+        setContentView(R.layout.activity_character_strengths_seen);
     }
 }
