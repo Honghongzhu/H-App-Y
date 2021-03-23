1e48dc3be3bedd9426669604ebe9a88d3f010118
Honghongzhu <honghong033@hotmail.com>
2021-03-23 14:34:11 +0100

    import project with working buttons

--- app/src/main/java/com/example/happy/MainActivity.java
+++ app/src/main/java/com/example/happy/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.happy;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,20 +13,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
-    public void launchRecommendationActivity(View view) {
+    public void launchRecommendationCSActivity(View view) {
+        Intent intent = new Intent(this, RecommendationCSActivity.class);
+        startActivity(intent);
     }
 
     public void launchTop10Activity(View view) {
+        Intent intent = new Intent(this, Top10Activity.class);
+        startActivity(intent);
     }
 
     public void launchHistoryActivity(View view) {
+        Intent intent = new Intent(this, HistoryActivity.class);
+        startActivity(intent);
     }
 
     public void launchSavedActivity(View view) {
+        Intent intent = new Intent(this, SavedActivity.class);
+        startActivity(intent);
     }
 
     public void launchSearchActivity(View view) {
+        Intent intent = new Intent(this, SearchActivity.class);
+        startActivity(intent);
     }
+
 }
--- app/src/main/java/com/example/happy/SearchActivity.java
+++ app/src/main/java/com/example/happy/SearchActivity.java
@@ -1,7 +1,6 @@
 package com.example.happy;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SearchActivity extends AppCompatActivity {
