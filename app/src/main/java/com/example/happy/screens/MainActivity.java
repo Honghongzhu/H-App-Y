317dd7049ce49613842a53285c210cfababb28f5
Javier Martinez <javimr98@gmail.com>
2021-05-21 16:23:11 +0200

    User ID implemented

--- app/src/main/java/com/example/happy/screens/MainActivity.java
+++ app/src/main/java/com/example/happy/screens/MainActivity.java
@@ -20,19 +20,27 @@ import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int currentUserId;
+
+    public int getCurrentUserId() {
+        return this.currentUserId;
+    }
+
+    public void setCurrentUserId (int currentUserId) {
+        this.currentUserId = currentUserId;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        int USER_ID;
 
         String androidId = Settings.Secure.getString(getContentResolver(),
                 Settings.Secure.ANDROID_ID);
 
         try {
             // check if the android id is already in the table
-            List<Users> result = Utils.executeQuery(
+            List<Users> usersTable = Utils.executeQuery(
                     Users.class,
                     MainActivity.this,
                     "select",
@@ -43,8 +51,8 @@ public class MainActivity extends AppCompatActivity {
             );
 
             // if it isn't, add it
-            if (result.toString().equals("[]")) {
-                List<NoResult> selectResult = Utils.executeQuery(
+            if (usersTable.toString().equals("[]")) {
+                List<NoResult> insertResult = Utils.executeQuery(
                         NoResult.class,
                         MainActivity.this,
                         "insert",
@@ -53,12 +61,20 @@ public class MainActivity extends AppCompatActivity {
                         "values",
                         String.format("(\'%s\')", androidId)
                 );
-
-            // if it is, store it in a global variable
-            } else {
-                USER_ID = result.get(0).getUserId();
             }
 
+            // Regardless of the case, get the ID
+            usersTable = Utils.executeQuery(
+                    Users.class,
+                    MainActivity.this,
+                    "select",
+                    "*",
+                    "users",
+                    "where",
+                    String.format("android_id=\'%s\'", androidId)
+            );
+
+            setCurrentUserId(usersTable.get(0).getUserId());
 
         } catch (ExecutionException e) {
             Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_LONG).show();
@@ -70,26 +86,31 @@ public class MainActivity extends AppCompatActivity {
 
     public void launchRecommendationCSActivity(View view) {
         Intent intent = new Intent(this, RecommendationCSActivity.class);
+        intent.putExtra("CURRENT_USER_ID", getCurrentUserId());
         startActivity(intent);
     }
 
     public void launchTop12Activity(View view) {
         Intent intent = new Intent(this, Top12Activity.class);
+        intent.putExtra("CURRENT_USER_ID", getCurrentUserId());
         startActivity(intent);
     }
 
     public void launchHistoryActivity(View view) {
         Intent intent = new Intent(this, HistoryActivity.class);
+        intent.putExtra("CURRENT_USER_ID", getCurrentUserId());
         startActivity(intent);
     }
 
     public void launchSavedActivity(View view) {
         Intent intent = new Intent(this, SavedActivity.class);
+        intent.putExtra("CURRENT_USER_ID", getCurrentUserId());
         startActivity(intent);
     }
 
     public void launchSearchActivity(View view) {
         Intent intent = new Intent(this, SearchActivity.class);
+        intent.putExtra("CURRENT_USER_ID", getCurrentUserId());
         startActivity(intent);
     }
 
--- app/src/main/java/com/example/happy/screens/Top12Activity.java
+++ app/src/main/java/com/example/happy/screens/Top12Activity.java
@@ -6,13 +6,22 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.happy.R;
 import com.example.happy.adapters.RankAdapter;
 import com.example.happy.data.Movie;
 import com.example.happy.data.MovieDatabase;
+import com.example.happy.queries.MovieInfo;
+import com.example.happy.queries.NoResult;
+import com.example.happy.queries.UserRatings;
+import com.example.happy.queries.Users;
+import com.example.happy.queries.Utils;
+import com.google.gson.Gson;
 
 import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class Top12Activity extends AppCompatActivity {
     private LinkedList<Movie> movieList;
@@ -25,8 +34,54 @@ public class Top12Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top12);
 
-        movieList = MovieDatabase.getAllMovies();
+        // Retrieve the current user of the app
+        int currentUserId = -1;
 
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            currentUserId = extras.getInt("CURRENT_USER_ID", -1);
+        }
+
+        try {
+            // Get the movies rated by the user
+            List<UserRatings> currentUserRatings = Utils.executeQuery(
+                    UserRatings.class,
+                    Top12Activity.this,
+                    "select",
+                    "*",
+                    "user_ratings",
+                    "where",
+                    String.format("user_id=%s", currentUserId)
+            );
+
+            // Get the info of every movie
+            List<MovieInfo> movieInfoTable = Utils.executeQuery(
+                    MovieInfo.class,
+                    Top12Activity.this,
+                    "select",
+                    "*",
+                    "movie_info",
+                    "",
+                    ""
+            );
+
+            String userRatingsJson = new Gson().toJson(currentUserRatings);
+            String movieInfoJson = new Gson().toJson(movieInfoTable);
+
+            // Now we call the python script that will return a string with ordered movie ids
+
+
+        } catch (ExecutionException e) {
+            Toast.makeText(Top12Activity.this, e.toString(), Toast.LENGTH_LONG).show();
+        } catch (InterruptedException e) {
+            Toast.makeText(Top12Activity.this, e.toString(), Toast.LENGTH_LONG).show();
+        }
+
+
+
+
+        /*
+        movieList = MovieDatabase.getAllMovies();
         // Get a handle to the RecyclerView
         recyclerView = findViewById(R.id.rv_top12);
         // Create an adapter and supply the data to be displayed
@@ -34,7 +89,7 @@ public class Top12Activity extends AppCompatActivity {
         // Connect the adapter with the RecyclerView
         recyclerView.setAdapter(adapter);
         // Give the RecyclerView a default layout manager
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));*/
     }
 
     public void launchRateActivity(View view) {
