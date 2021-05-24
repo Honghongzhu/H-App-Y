dba974ffd5b78205bc0f1c1049ec28b7a4dabd6b
Honghongzhu <honghong033@hotmail.com>
2021-05-24 11:11:23 +0200

    show top12 movies in recommendation

--- app/src/main/java/com/example/happy/screens/RecommendationActivity.java
+++ app/src/main/java/com/example/happy/screens/RecommendationActivity.java
@@ -17,7 +17,6 @@ import com.example.happy.queries.MovieRatings;
 import com.example.happy.queries.Utils;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -25,12 +24,8 @@ import java.util.stream.Collectors;
 
 public class RecommendationActivity extends AppCompatActivity {
 
-    //private LinkedList<MovieInfo> moviesToAdapt;
-    private List<MovieInfo> allMovieInfo;
     private List<MovieRatings> resultMovieRatings;
-    private List<MovieRatings> top12Movies;
-    private RecyclerView recyclerView;
-    private MovieAdapter adapter;
+    private List<MovieInfo> allMovieInfo;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -61,34 +56,41 @@ public class RecommendationActivity extends AppCompatActivity {
         }
 
         // Get the first 12 movies
-        top12Movies = resultMovieRatings.stream().limit(12).collect(Collectors.toList());
+        List<MovieRatings> top12Movies = resultMovieRatings.stream().limit(12).collect(Collectors.toList());
+        // Get movieId
+        ArrayList<String> top12MovieIds = new ArrayList<>();
+        for(MovieRatings mr : top12Movies) {
+            top12MovieIds.add(mr.getMovieId());
+        }
 
-        Toast.makeText(RecommendationActivity.this, top12Movies.get(0).getMovieId() + " " + top12Movies.get(0).getAverageEnjoyment(), Toast.LENGTH_LONG).show();
+        StringBuilder movieIDs = new StringBuilder();
+        for(String movieId : top12MovieIds) {
+            movieIDs.append(String.format("'%s', ", movieId));
+        }
+        movieIDs.delete(movieIDs.length()-2, movieIDs.length());
+        // Query all movies with the corresponding movieID
+        try {
+            allMovieInfo = Utils.executeQuery(
+                    MovieInfo.class,
+                    RecommendationActivity.this,
+                    "select",
+                    "movie_id, primary_title, start_year, poster_url",
+                    "movie_info",
+                    "WHERE movie_id in",
+                    String.format("(%s) ORDER BY FIELD(movie_id, %s)", movieIDs.toString(), movieIDs.toString())
+            );
+        } catch (ExecutionException e) {
+            Toast.makeText(RecommendationActivity.this, e.toString(), Toast.LENGTH_LONG).show();
+        } catch (InterruptedException e) {
+            Toast.makeText(RecommendationActivity.this, e.toString(), Toast.LENGTH_LONG).show();
+        }
 
+        LinkedList<MovieInfo> moviesSaved = new LinkedList<>(allMovieInfo);
 
-        // Query all movieInfo
-//        try {
-//            allMovieInfo = Utils.executeQuery(
-//                    MovieInfo.class,
-//                    RecommendationActivity.this,
-//                    "select",
-//                    "*",
-//                    "movie_info",
-//                    "",
-//                    ""
-//            );
-//        } catch (ExecutionException e) {
-//            Toast.makeText(RecommendationActivity.this, e.toString(), Toast.LENGTH_LONG).show();
-//        } catch (InterruptedException e) {
-//            Toast.makeText(RecommendationActivity.this, e.toString(), Toast.LENGTH_LONG).show();
-//        }
-
-
-
-//        recyclerView = findViewById(R.id.rv_recommendation);
-//        adapter = new MovieAdapter(this, allMovieInfo);
-//        recyclerView.setAdapter(adapter);
-//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        RecyclerView recyclerView = findViewById(R.id.rv_recommendation);
+        MovieAdapter adapter = new MovieAdapter(this, moviesSaved);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
     }
 
