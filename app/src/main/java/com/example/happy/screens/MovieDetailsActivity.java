af3d299fbe3db0b3bbc922cdaf4a943cdee3206d
Honghongzhu <honghong033@hotmail.com>
2021-05-25 17:48:42 +0200

    Add screen with movie details

---
+++ app/src/main/java/com/example/happy/screens/MovieDetailsActivity.java
@@ -0,0 +1,12 @@
+package com.example.happy.screens;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class MovieDetailsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
