c02412c5b672e15ca5da256d4e452e4fda9117ae
Honghongzhu <honghong033@hotmail.com>
2021-05-25 22:46:23 +0200

    Fix back button

--- app/src/main/java/com/example/happy/screens/MovieDetailsActivity.java
+++ app/src/main/java/com/example/happy/screens/MovieDetailsActivity.java
@@ -1,12 +1,34 @@
 package com.example.happy.screens;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import com.example.happy.R;
 
 public class MovieDetailsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_details);
+
+        // calling the action bar
+        ActionBar actionBar = getSupportActionBar();
+        // showing the back button in action bar
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
