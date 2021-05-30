6c084136ac92cf2d8867ad8139dd5d2382fbd4b5
Honghongzhu <honghong033@hotmail.com>
2021-05-30 19:27:51 +0200

    fix some minor bugs
    make font size of movie titles smaller, make more space for rank numbers and fixed back button in CSInfoScreen

--- app/src/main/java/com/example/happy/screens/InfoCSActivity.java
+++ app/src/main/java/com/example/happy/screens/InfoCSActivity.java
@@ -1,6 +1,7 @@
 package com.example.happy.screens;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,10 +14,11 @@ public class InfoCSActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cs_info);
+
+        // Show back button in actionbar
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
-//    public void launchCharacterStrengthsSeenActivity(View view) {
-//        Intent intent = new Intent(this, RecommendationCSActivity.class);
-//        startActivity(intent);
-//    }
 }
